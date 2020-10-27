2162760a3d4fc15f1840e945ceaa73345ce3224d
PastaSoba <sanukinomiyatuko09@gmail.com>
2020-10-28 01:54:28 +0900

    [新規] chenuを可視化するコードの作成（有効化すると、動作が非常に重くなる）

--- wu_physarum/server.py
+++ wu_physarum/server.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+# from collections import defaultdict
 
 from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
@@ -18,20 +18,66 @@ def agent_portrayal(agent):
     return portrayal
 
 
-class CustomCanvasGrid(CanvasGrid):
-    # CHANGED: render関数の動作を軽量化した
-    def render(self, model):
-        grid_state = defaultdict(list)
-        for agt in model.schedule.agents:
-            portrayal = self.portrayal_method(agt)
-            if portrayal:
-                portrayal["x"] = agt.pos[0]
-                portrayal["y"] = agt.pos[1]
-                grid_state[portrayal["Layer"]].append(portrayal)
-        return grid_state
+# class CustomCanvasGrid(CanvasGrid):
+#     # CHANGED: render関数の動作を軽量化した
+#     def __init__(
+#         self,
+#         portrayal_method,
+#         grid_width,
+#         grid_height,
+#         canvas_width=500,
+#         canvas_height=500,
+#     ):
+#         super().__init__(
+#             portrayal_method,
+#             grid_width,
+#             grid_height,
+#             canvas_width,
+#             canvas_height,
+#         )
+#         self.colormap = plt.get_cmap('viridis')
+#         self.maxiter = 100
+
+#     def get_color(self, chenu):
+#         return self.colormap(chenu / self.maxiter * 256)
+
+#     def __test(self, x, y, value):
+#         color = self.get_color(value)
+#         color_code = '#{}{}{}'.format(
+#             hex(int(color[0] + 0.5)),
+#             hex(int(color[1] + 0.5)),
+#             hex(int(color[2] + 0.5))
+#         ).replace('0x', '')
+#         return {
+#             "Shape": "rect",
+#             "w": 1,
+#             "h": 1,
+#             "x": x,
+#             "y": y,
+#             "Color": color_code,
+#             "Filled": "true",
+#         }
+
+#     def create_stage_portrayal(self, chenu_map_list):
+#         return [self.__test(i, j, val) for i, row in enumerate(chenu_map_list) for j, val in enumerate(row)]
+
+#     def render(self, model):
+#         grid_state = defaultdict(list)
+#         chenu_map_list = model.chenu_map.tolist()
+#         grid_state[1] = self.create_stage_portrayal(chenu_map_list)
+
+#         return grid_state
 
 
-grid = CustomCanvasGrid(
+# ltc_grid = CustomCanvasGrid(
+#     portrayal_method=agent_portrayal,
+#     grid_width=MODEL_PARAM["width"],
+#     grid_height=MODEL_PARAM["height"],
+#     canvas_width=600,
+#     canvas_height=600
+# )
+
+grid = CanvasGrid(
     portrayal_method=agent_portrayal,
     grid_width=MODEL_PARAM["width"],
     grid_height=MODEL_PARAM["height"],
