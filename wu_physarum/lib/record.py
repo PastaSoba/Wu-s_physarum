476714663e5242479be10a0668aeee861637e05c
PastaSoba <sanukinomiyatuko09@gmail.com>
2020-12-07 01:52:32 +0900

    [その他] recorderの説明を追加した

--- wu_physarum/lib/record.py
+++ wu_physarum/lib/record.py
@@ -1,3 +1,25 @@
+"""
+ModelRecorder
+
+# 役割
+ModelRecorderを通してモデルを実行することで、
+一定の間隔（ステップ）ごとにシミュレーションの
+出力を画像として保存することができる
+
+# 使用例
+recorder = ModelRecorder(
+    model=WuPhysarum,
+    datapoint_filename="twelve_ring.json",
+    seed=13647,                                            # モデルに与えるseed値
+    figure_folername="output_fig/twelve_ring/seed_13647",  # 画像を保存するディレクトリ名
+    max_iteration=20000,                                   # 最大試行回数
+    record_interval=200,                                   # 画像を保存する間隔（ステップ）
+)
+
+recorder.start()
+
+"""
+
 import os
 import time
 
